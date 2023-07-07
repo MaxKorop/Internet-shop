2ee6ece5d3d3018a789026c7820fd2ce2d0443ca
Max Korop <makskorop99@gmail.com>
2023-07-07 18:37:51 +0300

    Added .gitignore file and and started ignoring .env file and node_modules folder

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-app.listen(PORT, () => console.log(`Server started on port http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port http://localhost:${PORT}`));
