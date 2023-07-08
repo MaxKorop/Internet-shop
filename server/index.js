74b646811c4a45fc77c730a0c2bc54f2dc35c079
Max Korop <makskorop99@gmail.com>
2023-07-08 12:06:46 +0300

    Added models

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const sequelize = require("./db");
+const models = require("./models/models")
 
 const PORT = process.env.PORT || 3000;
 
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`Server started on port http://localhost:${PORT}`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 const app = express();
 
-app.listen(PORT, () => console.log(`Server started on port http://localhost:${PORT}`));
+start();
