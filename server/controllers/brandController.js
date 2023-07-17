ae7c362432b6f26d68cf4dc187a5c5842d7c553b
Max Korop <makskorop99@gmail.com>
2023-07-17 17:57:38 +0300

    Added error handling, controllers for routers, morgan logger, fileupload  and static files, changed routers

---
+++ server/controllers/brandController.js
@@ -0,0 +1,17 @@
+const { Brand } = require("../models/models");
+const ApiError = require("../error/ApiError");
+
+class BrandController{
+    async create(req, res) {
+        const { name } = req.body;
+        const brand = await Brand.create({ name });
+        return res.status(200).json(brand);
+    }
+
+    async getAll(req, res) {
+        const brands = await Brand.findAll();
+        return res.status(200).json(brands);
+    }
+}
+
+module.exports = new BrandController();
--- server/index.js
+++ server/index.js
@@ -3,17 +3,25 @@ const express = require("express");
 const sequelize = require("./db");
 const models = require("./models/models");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const morgan = require("morgan");
 const router = require("./routes/index");
-
+const errorHandler = require("./middleware/ErrorHandlingMiddleWare");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use('/api', router)
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
+app.use('/api', router);
+
+app.use(errorHandler);
 
 const start = async () => {
     try {
